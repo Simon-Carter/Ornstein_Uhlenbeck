ffbaf70242b9d450cf76621f25964deefc894982
Simon <simondcarter@hotmail.com>
2022-10-03 10:32:00 -0400

    data now stored as an array of chains in the program added file io implementation to store variables as jld2 files for later analysis

--- thermal.jl
+++ thermal.jl
@@ -3,6 +3,7 @@ using Plots, StatsPlots
 using Distributions, Random, FFTW
 using Turing, ReverseDiff, Memoization, LinearAlgebra
 using Base.Threads
+using FileIO, JLD2
 
 #defining the models
 Turing.setadbackend(:reversediff)
@@ -26,7 +27,7 @@ end
 @model oupn_thermal(rn,T,delta_t,::Type{R}=Vector{Float64}) where {R} = begin
     ampl ~ Uniform(0.0,2.0)
     tau ~ Uniform(0.1,2.0)
-    noise_ampl ~ Uniform(0.0,0.5)
+    noise_ampl ~ truncated(Normal(0.2,0.3), lower=0)
     
     b = exp(-delta_t/tau)
     r = R(undef, T)
@@ -43,7 +44,7 @@ end
 @model oupn_mult(rn,T,delta_t,::Type{R}=Vector{Float64}) where {R} = begin
     ampl ~ Uniform(0.0,2.0)
     tau ~ Uniform(0.1,2.0)
-    noise_ampl ~ Uniform(0.0,0.5)
+    noise_ampl ~ Exponential(4)
     
     b = exp(-delta_t/tau)
     r = R(undef, T)
@@ -65,7 +66,7 @@ function sample_model(dt, Tnoise, oupn)
 #Tnoise = 0.2
 #dt = 0.05:0.05:2.0
 W = OrnsteinUhlenbeckProcess(Θ,μ,σ,0.0,1.0)
-prob = NoiseProblem(W,(0.0,100.0))
+prob = NoiseProblem(W,(0.0,50.0))
 sol = [solve(prob;dt=i).u for i in dt]
 
 #generate  thermal noise (uncomment this block and comment out multiplicative block)
@@ -79,25 +80,25 @@ for i in eachindex(dt)
 end
 noisedata50 = sol
 
-@time oupndata = [sample(oupn(noisedata50[i],length(noisedata50[i]),dt[i]), NUTS(0.65), 1000) for i in eachindex(dt)]
+@time oupndata = [sample(oupn(noisedata50[i],length(noisedata50[i]),dt[i]), NUTS(0.65), 5000) for i in eachindex(dt)]
 return oupndata
 
 end
 
 sample_rate = 0.1:0.1:0.1
+num_cycles = 2
 mean_total = [Threads.Atomic{Float64}(0) for i in sample_rate]
 std_total = [Threads.Atomic{Float64}(0) for i in sample_rate]
-@threads for i in 1:10
-    data = sample_model(sample_rate, 0.2, oupn_mult)
-    for j in eachindex(mean_total)
-        atomic_add!(mean_total[j], mean(data[j][:noise_ampl]))
-        atomic_add!(std_total[j], std(data[j][:noise_ampl]))
-    end
+data_total = Array{Chains}(undef, num_cycles, length(sample_rate))
+@threads for i in 1:num_cycles
+    data_total[i,:] = sample_model(sample_rate, 0.2, oupn_mult)
 end
 
-mean_total2 = [i[] for i in mean_total]/length(mean_total)
-std_total2 = [i[] for i in std_total]/length(std_total)
+#mean_total2 = [i[] for i in mean_total]/length(mean_total)
+#std_total2 = [i[] for i in std_total]/length(std_total)
 
-plot(sample_rate, mean_total2, yerr = std_total2, title = "Thermal noise amplitude", xlabel = "sample rate in units of the relaxation time")
+#plot(sample_rate, mean_total2, yerr = std_total2, title = "Thermal noise amplitude", xlabel = "sample rate in units of the relaxation time")
 
-savefig("thermal.png")
+#savefig("thermal.png")
+
+FileIO.save("mult_exp_4.jld2","data",data_total)
