934b132cdf180b4857cf37bdc08dc23ec648eb54
Simon <simondcarter@hotmail.com>
2022-11-26 22:36:21 -0500

    added support for parcelling all ROI,    - total contains the fmri data for all regions of interest, need to rename and clean up some code,

--- nifti_parse.jl
+++ nifti_parse.jl
@@ -3,41 +3,42 @@ voxel_space = niread("sample_data/processed_voxel_space/sub-108_ses-bhb_task-res
 atlas = niread("sample_data/ROIs_300inVol_MNI.nii");
 println(size(atlas))
 
-# create an array the same size as the atlas
-d = findall(x -> x == 1.0, atlas.raw)
+function get_roi(voxel_space, region)
+    d = findall(x -> x == region, atlas.raw)
 
-d = getindex.(d, [1 2 3 4])
-d[:,1:3] = d[:,1:3] .- 1
+    d = getindex.(d, [1 2 3 4])
+    d[:,1:3] = d[:,1:3] .- 1
 
-#transformations
+    #transformations
 
-a = getaffine(atlas.header)
-b = inv(getaffine(voxel_space.header))
-transform = b*a
+    a = getaffine(atlas.header)
+    b = inv(getaffine(voxel_space.header))
+    transform = b*a
 
-data = []
+    data = []
 
-temp = Array{Float64}(undef,size(d, 1))
+    temp = Array{Float64}(undef,size(d, 1))
 
-td = (transform * (d'))'
+    td = (transform * (d'))'
 
-td = round.(Int, td[:, 1:3])
+    td = round.(Int, td[:, 1:3])
 
-td = td .+ 1
+    td = td .+ 1
 
-temp = Array{Float64}(undef,size(d, 1))
-k=1
-for k in 1:size(voxel_space, 4)
-    for i in eachindex(temp)
-        temp[i] = voxel_space[td[i,:]..., k]
+    temp = Array{Float64}(undef,size(d, 1))
+    k=1
+    for k in 1:size(voxel_space, 4)
+        for i in eachindex(temp)
+            temp[i] = voxel_space[td[i,:]..., k]
+        end
+
+       #println(k, temp)
+
+        push!(data, copy(temp))
+
     end
 
-    #println(k, temp)
-
-    push!(data, copy(temp))
-
+    return data
 end
 
-
-
-#push!(data, Array{Float64}(undef,size(d, 1)))
+total = [get_roi(voxel_space, i) for i in 1:300]
