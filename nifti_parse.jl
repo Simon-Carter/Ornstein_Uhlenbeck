85ae5c0927cae3110e1f12a9b7dc512a31445749
Simon <simondcarter@hotmail.com>
2022-11-26 22:26:09 -0500

    Fixed indexing issue added support for time

--- nifti_parse.jl
+++ nifti_parse.jl
@@ -1,6 +1,5 @@
 using NIfTI
 voxel_space = niread("sample_data/processed_voxel_space/sub-108_ses-bhb_task-rest_run-1.nii");
-println(size(ni))
 atlas = niread("sample_data/ROIs_300inVol_MNI.nii");
 println(size(atlas))
 
@@ -8,6 +7,7 @@ println(size(atlas))
 d = findall(x -> x == 1.0, atlas.raw)
 
 d = getindex.(d, [1 2 3 4])
+d[:,1:3] = d[:,1:3] .- 1
 
 #transformations
 
@@ -23,9 +23,19 @@ td = (transform * (d'))'
 
 td = round.(Int, td[:, 1:3])
 
+td = td .+ 1
+
 temp = Array{Float64}(undef,size(d, 1))
-for i in eachindex(temp)
-    temp[i] = voxel_space[td[i,:]..., 1]
+k=1
+for k in 1:size(voxel_space, 4)
+    for i in eachindex(temp)
+        temp[i] = voxel_space[td[i,:]..., k]
+    end
+
+    #println(k, temp)
+
+    push!(data, copy(temp))
+
 end
 
 
